90df01d4c3c0cbd03277d143046fef1a78152e7f
Javier Prieto <79945638+jprietor@users.noreply.github.com>
2021-03-07 00:38:58 +0100

    Added multi deployments and blue/green switch

--- vars/pipelineForApiSampleJavaMaster.groovy
+++ vars/pipelineForApiSampleJavaMaster.groovy
@@ -1,3 +1,16 @@
+def deployApp(newDeployment, kubeconfig) {
+    newDeployment.metadata.name = "api-sample-java-${env.VERSION}"
+    newDeployment.metadata.labels.version = "${env.VERSION}"
+    newDeployment.spec.selector.matchLabels.version = "${env.VERSION}"
+    newDeployment.spec.template.metadata.labels.version = "${env.VERSION}"
+    newDeployment.spec.template.spec.containers[0].image = "${env.DOCKER_IMAGE}:${env.VERSION}"
+
+    sh "rm deployment.yaml"
+    writeYaml file: "deployment.yaml", data: newDeployment
+
+    sh "kubectl --kubeconfig ${kubeconfig} apply -f deployment.yaml"
+}
+
 def call(env){
     pipeline {
         agent none
@@ -75,12 +88,25 @@ def call(env){
                                 returnStdout: true
                             )
 
-                            if (deployments.toInteger() == 1) {
-                                // Delete previous deployment and deploy new version...
-                                println "TODO: redeployment"
+                            if (deployments.toInteger() >= 2) {
+                                def deploymentToDelete = sh(
+                                    script: "kubectl --kubeconfig ${kubeconfig} get deploy --no-headers --sort-by=.metadata.creationTimestamp -l app=api-sample-java | head -1 | awk '{print \$1}'",
+                                    returnStdout: true
+                                )
 
-                            }else if (deployments.toInteger() == 0) {
-                                // Deploy new service
+                                sh(
+                                    script: "kubectl --kubeconfig ${kubeconfig} delete deploy ${deploymentToDelete}"
+                                )
+
+                                // Deploy new version
+                                deployApp(newDeployment, kubeconfig)
+
+                            } else if (deployments.toInteger() == 1) {
+                                // Deploy new version
+                                deployApp(newDeployment, kubeconfig)
+
+                            } else if (deployments.toInteger() == 0) {
+                                // Deploy service
                                 sh(
                                     script: "kubectl --kubeconfig ${kubeconfig} create -f manifests/deployment.yaml"
                                 )
@@ -88,6 +114,42 @@ def call(env){
                                     script: "kubectl --kubeconfig ${kubeconfig} create -f manifests/service.yaml"
                                 )
                             }
+                        }
+                    }
+                }
+            }
+            stage('Do Blue/Green') {
+                agent {
+                    docker { 
+                        label 'docker'
+                        image 'bitnami/kubectl' 
+                        args '--entrypoint=""'
+                    }
+                }
+                steps {
+                    script {
+                        input message: 'Do you want to switch environment?', ok: 'Switch!'
+                        def patch = readYaml file: 'manifests/service-patch.yaml'
+                        def deployVersion
+                        def actualVersion
+                        
+
+                        withCredentials([file(credentialsId: 'kubeconfig', variable: 'kubeconfig')]) {
+                            actualVersion = readYaml text: sh(script: "kubectl --kubeconfig ${kubeconfig} get svc ${env.SVC_NAME} -o yaml", returnStdout: true)
+                            actualVersion = actualVersion.spec.selector.version
+                            deployVersion = readYaml text: sh(script: "kubectl --kubeconfig ${kubeconfig} get deployment -l version!=${actualVersion} -o yaml", 
+                                                                returnStdout: true)
+                        }
+                        
+                        deployVersion = deployVersion.items[0].metadata.labels.version
+
+                        patch.spec.selector.version = deployVersion
+
+                        sh "rm patch.yaml"
+                        writeYaml file: 'patch.yaml', data: patch
+
+                        withCredentials([file(credentialsId: 'kubeconfig', variable: 'kubeconfig')]) {
+                            sh(script: "kubectl --kubeconfig ${kubeconfig} patch service ${env.SVC_NAME} --patch \"\$(cat patch.yaml)\"")
                         }
                     }
                 }
